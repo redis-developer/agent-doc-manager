3b10f7c5ad0c2b42115f3864131cd2ef2030901a
Will Johnston <will-github@wwj.dev>
2025-09-12 11:39:27 -0700

    feat: limiting to 5 docs

--- server/components/crawl/controller.ts
+++ server/components/crawl/controller.ts
@@ -65,7 +65,7 @@ export async function crawlUrl(
   url: string,
   instructions: string,
 ) {
-  return await crawlLocalDocuments(userId, projectId);
+  return await crawlLocalDocuments(userId, projectId, 5);
 
   // const pages = await tavily.crawl(url, instructions);
 
